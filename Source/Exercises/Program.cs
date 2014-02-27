0d4f1930f0ea4b1c2924afb52bc3ea1a9034f274
unknown <roho@CPH-R9R878X.efif.dk>
2014-02-27 17:23:20 +0100

    Update

--- Source/Exercises/Program.cs
+++ Source/Exercises/Program.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using Exercises.Exercise3;
 using Exercises.Exercise7;
+using Exercises.Exercise9a;
 
 namespace Exercises {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Set me to the correct project");
-            
+
+            MyCode c = new MyCode();
+            c.MyMain();
         }
     }
 }
