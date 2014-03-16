add10ba854d2acfda6d78546f7bc3a5c73a79695
unknown <roho@CPH-R9R878X.efif.dk>
2014-03-16 17:22:50 +0100

    Update

--- Source/Exercises/Exercise09b/FruitBoxSolution.cs
+++ Source/Exercises/Exercise09b/FruitBoxSolution.cs
@@ -5,9 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercises.Exercise09b {
+    class MyCode {
+        public void MyMain() {
+            // 2a
+            FruitBoxSolution f = new FruitBoxSolution(1.99, 2.99);
+
+            // 2b
+            Console.WriteLine(f.GetBoxContentDescription());
+
+            // 2c
+            f.AddApples(3);
+            f.AddBananas(5);
+
+            // 2d
+            Console.WriteLine(f.GetBoxContentDescription());
+
+            // 3
+            Console.WriteLine(f.GetTotalPrice());
+
+            // 4
+            f.SetPriceForOneApple(9);
+            f.SetPriceforOneBanana(10);
+            Console.WriteLine(f.GetTotalPrice());
+        }
+    }
+
     // This class represents a simple box of fruit - it can only contain
     // Bananas and Apples. It also represents the price of each item. 
-    class FruitBox {
+    class FruitBoxSolution {
         private int numberOfBananas;
         private int numberOfApples;
         private double priceForOneBanana;
@@ -15,11 +40,27 @@ namespace Exercises.Exercise09b {
 
         // Create the fruit box - it is empty to start with, but the prices must
         // be specified
-        public FruitBox(double priceForOneBanana, double priceForOneApple) {
+        public FruitBoxSolution(double priceForOneBanana, double priceForOneApple) {
             this.priceForOneBanana = priceForOneBanana;
             this.priceForOneApple = priceForOneApple;
             this.numberOfBananas = 0;
             this.numberOfApples = 0;
+        }
+
+        // 3
+        public double GetTotalPrice() {
+            return numberOfBananas * priceForOneBanana +
+                   numberOfApples * priceForOneApple;
+        }
+
+        // 4
+        public void SetPriceForOneApple(double price) {
+            priceForOneApple = price;
+        }
+
+        // 4
+        public void SetPriceforOneBanana(double price) {
+            priceForOneBanana = price;
         }
 
         public void AddApples(int numberOfApples) {
--- Source/Exercises/Program.cs
+++ Source/Exercises/Program.cs
@@ -1,5 +1,4 @@
-﻿using Exercises.Exercise09b;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +7,7 @@ using System.Threading.Tasks;
 namespace Exercises {
     class Program {
         static void Main(string[] args) {
-            Runner r = new Runner();
-            r.Run();
+
         }
     }
 }
