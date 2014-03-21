330ffb84d98c537042dab879152701c3a7953498
unknown <roho@CPH-R9R878X.efif.dk>
2014-03-21 10:36:03 +0100

    Updated

--- Source/Exercises/Program.cs
+++ Source/Exercises/Program.cs
@@ -1,4 +1,4 @@
-﻿using Exercises.Exercise16;
+﻿using Exercises.Exercise13;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,44 +8,8 @@ using System.Threading.Tasks;
 namespace Exercises {
     class Program {
         static void Main(string[] args) {
-            int noOfChildren = 5;
-            int childSupport = 0;
-
-            switch (noOfChildren) {
-                case 0:
-                    childSupport = 0;
-                    break;
-                case 1:
-                    childSupport = 1200;
-                    break;
-                case 2:
-                    childSupport = 2000;
-                    break;
-                case 3:
-                    childSupport = 2600;
-                    break;
-                default:
-                    childSupport = 3000;
-                    break;
-            }
-
-            if (noOfChildren == 0) {
-                childSupport = 0;
-            }
-            else if (noOfChildren == 1) {
-                childSupport = 1200;
-            }
-            else if (noOfChildren == 2) {
-                childSupport = 2000;
-            }
-            else if (noOfChildren == 3) {
-                childSupport = 2600;
-            }
-            else if (noOfChildren > 3) {
-                childSupport = 3000;
-            }
-
-            Console.WriteLine(childSupport);
+            Runner r = new Runner();
+            r.Run();
         }
     }
 }
