196aea2297a81e30ea84a89660dc48f96cf18505
unknown <roho@CPH-R9R878X.efif.dk>
2014-04-04 08:45:49 +0200

    Updated

---
+++ Source/Exercises/Exercise22/BookCatalog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercise22 {
+    class Runner {
+        public void MyCode() {
+            Book b1 = new Book("AD1337", "Java for All", "John Potter", 352);
+            Book b2 = new Book("XS3220", "Gardening", "Alex Sohn", 220);
+            Book b3 = new Book("DD0095", "Cars in the USA", "Susan Dreyer", 528);
+            Book b4 = new Book("PT1295", "Copenhagen Dawn", "Dan Mygind", 104);
+
+            BookCatalog theCatalog = new BookCatalog();
+            theCatalog.AddBook(b1);
+            theCatalog.AddBook(b2);
+            theCatalog.AddBook(b3);
+            theCatalog.AddBook(b4);
+
+            Console.WriteLine("------------- Printing all books ---------------");
+            theCatalog.PrintAllBooks();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine();
+
+            SafePrintBookInformation(theCatalog.LookupBook("AD1337"));
+            SafePrintBookInformation(theCatalog.LookupBook("AD1338"));
+            SafePrintBookInformation(theCatalog.LookupBook("PT1295"));
+            SafePrintBookInformation(theCatalog.LookupBook("......"));
+            SafePrintBookInformation(theCatalog.LookupBook("ad1337"));
+        }
+
+        public void SafePrintBookInformation(Book aBook) {
+            if (aBook == null) {
+                Console.WriteLine("Book was null...");
+            } else {
+                Console.WriteLine(aBook.GetAllInformation());
+            }
+        }
+    }
+
+    class BookCatalog {
+        private List<Book> books;
+
+        public BookCatalog() {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book aBook) {
+            // Add code that can add the given Book object to the list
+        }
+
+        public void PrintAllBooks() {
+            // Add code that can print all books in the list
+            // Hint: You will need a repetition statement
+        }
+
+        public Book LookupBook(string isbn) {
+            Book matchingBook = null;
+
+            // Add code that will find a book (if any) in the list
+            // which has a matching ISBN number. The variable matchingBook
+            // should be set to this book
+            // Hint: You will need a repetition statement
+
+            return matchingBook;
+        }
+    }
+}
---
+++ Source/Exercises/Exercise22/README.md
@@ -0,0 +1,26 @@
+﻿# Exercise 22: Introducing lists
+
+This exercise illustrates the concept of a *catalog*. A catalog 
+is a class that can store and use data of a certain type, without 
+revealing the specific representation of data to the user of 
+the catalog.
+
+The project contains the simple data class Book (note however 
+that we consider the instance field isbn to be a "key" for Book,
+i.e. no two Book objects can have the same isbn value). Also, it 
+contains the (incomplete) catalog class BookCatalog. The three 
+public methods in BookCatalog allow the user to store and use 
+Book objects in a simple way (see the comments in the code for 
+more details about each method).
+
+Given that we have chosen to use a List<Book> to store Book 
+objects internally in the BookCatalog class your job is now:
+
+  1. Complete the three methods in the BookCatalog class.
+  2. Study the test written in the runner class and figure 
+     out what you expect the test to output.
+  3. Run the program and see if the output of the test matches 
+     your expectations (if not, you will have to examine the 
+	 test and your code once again)
+  4. Is there anything in the program that prevents a user 
+     from entering two Book objects with the same isbn value?
