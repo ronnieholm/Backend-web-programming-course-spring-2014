3bf9c7f548359ec8671e54ed19813aea57c149f6
unknown <roho@CPH-R9R878X.efif.dk>
2014-04-11 16:17:44 +0200

    Updated

---
+++ Source/Exercises/Exercise30/PartnerDesigner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercise30 {
+    class Runner {
+        public void Run() {
+            Human h1 = new Human(true, "Brown", "Black", 1);
+            Human h2 = new Human(false, "White", "Blue", 4);
+            Human h3 = new Human(true, "Blond", "Green", 8);
+
+            Console.WriteLine(h1.GetDescription());
+            Console.WriteLine(h2.GetDescription());
+            Console.WriteLine(h3.GetDescription());
+        }
+    }
+
+    class Human {
+        private bool maleOrFemale;
+        private string eyeColor;
+        private string hairColor;
+        private int heightCategory;
+
+        public Human(bool maleOrFemale, string eyeColor, string hairColor, int heightCategory) {
+            this.maleOrFemale = maleOrFemale;
+            this.eyeColor = eyeColor;
+            this.hairColor = hairColor;
+            this.heightCategory = heightCategory;
+        }
+
+        public string GetDescription() {
+            string description = "You got a " + GetGenderDescription();
+            description = description + ", with " + eyeColor + " eyes";
+            description = description + ", " + hairColor + " hair";
+            description = description + ", who is " + GetHeightDescription();
+            return description;
+        }
+
+        public string GetGenderDescription() {
+            if (maleOrFemale) {
+                return "man";
+            }
+            else {
+                return "woman";
+            }
+        }
+
+        public string GetHeightDescription() {
+            switch (heightCategory) {
+                case 0:
+                    return "Very short";
+                case 1:
+                    return "Short";
+                case 2:
+                    return "Medium height";
+                case 3:
+                    return "Tall";
+                case 4:
+                    return "Very tall";
+                default:
+                    return "Unknown height";
+            }
+        }
+    }
+}
---
+++ Source/Exercises/Exercise30/PartnerDesignerSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Exercise30 {
+    class SolutionRunner {
+        public void Run() {
+            HumanSolution h1 = new HumanSolution(GenderSolution.Male, EyeColorSolution.Brown, HairColorSolution.Black, HeightCategorySolution.Short);
+            HumanSolution h2 = new HumanSolution(GenderSolution.Female, EyeColorSolution.Green, HairColorSolution.Blond, HeightCategorySolution.VeryTall);
+
+            Console.WriteLine(h1.GetDescription());
+            Console.WriteLine(h2.GetDescription());
+        }
+    }
+
+    enum GenderSolution { Male, Female };
+    enum EyeColorSolution { Green, Blue, Black, Brown };
+    enum HairColorSolution { Brown, Black, Blond };
+    enum HeightCategorySolution { VeryShort, Short, MediumHeight, Tall, VeryTall };
+
+    class HumanSolution {
+        private GenderSolution gender;
+        private EyeColorSolution eyeColor;
+        private HairColorSolution hairColor;
+        private HeightCategorySolution heightCategory;
+
+        public HumanSolution(GenderSolution gender, EyeColorSolution eyeColor, HairColorSolution hairColor, HeightCategorySolution heightCategory) {
+            this.gender = gender;
+            this.eyeColor = eyeColor;
+            this.hairColor = hairColor;
+            this.heightCategory = heightCategory;
+        }
+
+        public string GetDescription() {
+            string description = "You got a " + gender;
+            description = description + ", with " + eyeColor + " eyes";
+            description = description + ", " + hairColor + " hair";
+            description = description + ", who is " + heightCategory;
+            return description;
+        }
+    }
+}
